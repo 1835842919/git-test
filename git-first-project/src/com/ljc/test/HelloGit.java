b841b4bdffcd6e53cc52db7059e53a7d6c8f3479
lijiac <1835842919@qq.com>
2021-02-26 13:51:32 +0800

    修改了HelloGit.java文件

--- git-first-project/src/com/ljc/test/HelloGit.java
+++ git-first-project/src/com/ljc/test/HelloGit.java
@@ -9,8 +9,10 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //我做了修改
     }
     public void test(){
+        int i = 1;
         System.out.println("666");
     }
 }
